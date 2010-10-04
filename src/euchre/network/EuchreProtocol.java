52a8f4f8df75e30834fede38223056fb5767e2cd
matt.helgen <matt.helgen@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-04 21:33:13 +0000

    Initial network classes

---
+++ src/euchre/network/EuchreProtocol.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package euchre.network;
+
+/**
+ * @author mdhelgen
+ * Facilitates network communication between the clients and the server. 
+ */
+public class EuchreProtocol {
+	
+	
+	/**
+	 * Get any necessary references
+	 * 
+	 */
+	public EuchreProtocol(){
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/euchre/network/EuchreServer.java
@@ -0,0 +1,43 @@
+package euchre.network;
+
+import java.net.ServerSocket;
+
+/**
+ * Holds information about the server
+ * 
+ * @author mdhelgen
+ *
+ */
+public class EuchreServer {
+	
+	/**
+	 * Get any necessary references
+	 * 
+	 * @param name The name of the server (placeholder)
+	 * @param s The socket connection to the server
+	 */
+	public EuchreServer(String name, ServerSocket s){
+		
+		
+	}
+	
+	/**
+	 * Get the name of the server
+	 * 
+	 * @return the name of the server
+	 */
+	public String getName(){
+		return null;
+	}
+	
+	/**
+	 * Get a reference to the server connection
+	 * 
+	 * @return the socket connection of the server
+	 */
+	public ServerSocket socket(){
+		
+		return null;
+	}
+
+}
