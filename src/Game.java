711cfe4d5935bf7659f3f35d6b6a617afbfee836
unwrittenrainbow <unwrittenrainbow@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-04 21:28:33 +0000

    Completed the Game class, only javadoc and comments.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,20 @@
-
+/**
+ * 
+ * @author Timothy Ward
+ * 
+ * This class is the highest order class of the Euchre program. It is responsible for the highest level interactions between componenets.
+ *
+ */
 public class Game {
+	
+	/**
+	 * This method is the first method called in the program. This method is 
+	 * responsible for instantiating all objects and running the overall program.
+	 * 
+	 * @param args A String array.
+	 */
+	public void main(String [] args){
+		
+	}
 
 }
