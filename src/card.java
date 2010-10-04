c66fdad91008eb6771dd519609b1446d1c790fad
samwilke <samwilke@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-04 22:08:02 +0000

    card and deck classes completed

---
+++ src/card.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * @author sdwilke
+ * 
+ */
+
+public class card{
+	char suit;		//h = hearts, s = spades, c = clubs, d = diamonds
+	char value;		// 9, 0 (0 = 10), a = ace, j = jack, q = queen, k = king
+	
+	/**
+	 * creates a card object with the value and suit indicated
+	 * 
+	 * @param cardSuit	suit of the card
+	 * @param cardValue	value on the card
+	 */
+	
+	public card(char cardValue, char cardSuit){
+		suit = cardSuit;
+		value = cardValue;
+	}
+	
+	/**
+	 * retrieves suit of card (ie. h, s, c, d)
+	 * @return character representing suit of card
+	 */
+	
+	public char getSuit(){
+		return suit;
+	}
+	
+	/**
+	 * retrieves value on card (ie. a, 9, 0, j, q, k)
+	 * @return character representing value on card
+	 */
+	
+	public char getCardValue(){
+		return value;
+	}
+}
