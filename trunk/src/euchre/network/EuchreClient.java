b9726b1524d1205a1e4a190784338b6160d3800d
matt.helgen <matt.helgen@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-04 21:33:13 +0000

    Initial network classes

---
+++ trunk/src/euchre/network/EuchreClient.java
@@ -0,0 +1,24 @@
+package euchre.network;
+
+import java.net.ServerSocket;
+
+/**
+ * Holds network information about the client
+ * 
+ * @author mdhelgen
+ *
+ */
+public class EuchreClient {
+
+	/**
+	 * Get any necessary references
+	 * 
+	 */
+	public EuchreClient(String name, ServerSocket s){
+		
+		
+	}
+	
+	
+	
+}
---
+++ trunk/src/euchre/network/NetworkManager.java
@@ -0,0 +1,58 @@
+package euchre.network;
+
+import java.util.LinkedList;
+
+/**
+ * Listens for connections, keeps track of existing connections
+ * 
+ * @author mdhelgen
+ *
+ */
+public class NetworkManager {
+
+	/**
+	 * set up any necessary references, keep track of own network name
+	 * 
+	 */
+	public NetworkManager(){
+		
+	}
+	
+	
+	/**
+	 * Spawn a new thread for a new client
+	 * 
+	 */
+	public void addClient(){
+		
+		
+	}
+	
+	/**
+	 * Set the location of the server
+	 * 
+	 */
+	public void setServer(){
+		
+		
+	}
+	
+	/**
+	 * Get the list of clients
+	 * 
+	 */
+	public LinkedList<EuchreClient> getClientList(){
+		
+		return null;
+	}
+	
+	/**
+	 * Get the server
+	 * 
+	 */
+	public EuchreServer getServer(){
+		
+		return null;
+	}
+	
+}
