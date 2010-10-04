23d3bb8bbcbe106121000e1819f9ba7d9e3b656e
kylekary <kylekary@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-04 21:44:04 +0000

    Add methods

--- trunk/src/AI.java
+++ trunk/src/AI.java
@@ -6,20 +6,123 @@
 
 
 /**
- * Imports
- *  
+ * Imports???
+ * 
  */
 
 
 public class AI {
+	
+	
+	private card[] hand; //The AI's hand of cards
+	private card card1, card2, card3; //Card played by player to the AI's left, AI's partner, and AI's right, respectively
+	private boolean isTurn = false;
+	private char trump; //Trump suit: s=spades, h=hearts, d=diamonds, c=clubs
+	private int round; //Which round of the hand it currently is (1,2,3,4,5)
+	private int tricks;  //Number of tricks won by the AI and its partner
 
 	/**
-	 * The main method.
+	 * The main method. Will probably end up waiting here for its turn, then 
+	 * calling other methods to complete its turn.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+//		while(isTurn==true){
+//			if(round==1 && trump != null){
+//				orderUp();
+//			}
+//			else{
+//				
+//			}
+//
+//		}
 
 	}
+	
+	/**
+	 * Determines if the AI will order up the suit or pass on the trump suit, 
+	 * and acts accordingly. Should only be called once per hand.
+	 */
+	public void orderUp(){
+		
+	}
+	
+	/**
+	 * Determines the best card to lead and plays it.
+	 */
+	public void leadCard(){
+		
+		//if hand contains right bower, play right bower
+		//else if hand contains left bower, play left bower
+		//else if hand contains off-suit, play highest off-suit
+		//else play lowest trump
+		
+	}
+	
+	/**
+	 * Method for when the AI must follow another card. Determines the best card to play and plays it.
+	 */
+	public void followCard(){
+		
+		//if hasSuit, check if partner has trick
+			//if partnerHasTrick, play lowest same-suit
+			//if !partnerHasTrick, play highest same suit
+		//if !hasSuit, check if partner has trick
+			//if !partnerHasTrick && AI has trump, play lowest trump
+			//else play lowest off-suit
+
+	}
+	
+	/**
+	 * Plays the given card.
+	 * @param c The card to be played by the AI.
+	 */
+	public void playCard(card c){
+		
+	}
+	
+	/**
+	 * Determines if the AI has to follow suit with the lead card.
+	 * @return True if the AI has suit, false if it does not.
+	 */
+	public boolean hasSuit(){
+		
+	}
+	
+	/**
+	 * Determines if the AI's partner is going to take the trick
+	 * @return True if the partner is going to take it, false otherwise
+	 */
+	public boolean partnerHasTrick(){
+		
+	}
+	
+	/**
+	 * Finds the lowest card in the AI hand.
+	 * @return the lowest card in the AI hand.
+	 */
+	public card lowestCard(){
+		
+	}
+	
+	/**
+	 * Finds the lowest trump card in the AI hand.
+	 * @return the lowest trump card in the AI hand.
+	 */
+	public card lowestTrump(){
+		
+	}
+	
+	/**
+	 * Finds the highest off-suit card in the AI hand.
+	 * @return the highest off-suit card in the AI hand.
+	 */
+	public card highestOffSuit(){
+		
+	}
+	
+	
+	
 
 }
