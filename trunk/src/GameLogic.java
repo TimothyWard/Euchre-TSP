95b7ff88b680e1f1691869350ccc2bf455ed0b5b
unwrittenrainbow <unwrittenrainbow@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-05 00:16:06 +0000

    Changed a lot of classes, names, and method information. Also coded a few methods.

--- trunk/src/GameLogic.java
+++ trunk/src/GameLogic.java
@@ -1,46 +1,19 @@
-
-
-
-
 /**
  * 
  * @author Timothy Ward
  * 
  * This class is solely responsible for game operations that pertain to the logic of the game.
- * Its functions include determining the winner of a round, determining the winner of a trick,
- *  determining the number of points to be awarded to a team for a specific trick and so on.
  *
  */
 public class GameLogic{
-	
-	private int scoreOfWe =0;
-	private int scoreOfThey =0;
-	/**
-	 * This method is just a place holder in case anything should need to be done in it.
-	 * Comments will be more filled in later once actions to be taken in this method are known.
-	 * @param args A String array.
-	 */
-	public void main(String [] args){
-		
-	}
 
 	/**
-	 * This method accepts a string formated to contain precisely who did what when in the round.
-	 * This method is responsible for determining what happened when and who did what in a manner 
-	 * that will determine, decide and catalog the winner of that round, and the number of points
-	 * to be assigned for that round to the winning team.
-	 * @param roundSequence The sequence of events in a formatted string that happened in this round.
+	 * This method accepts a round object and interprets it relative to the game.
+	 * @param roundSequence The round object containing all information for a specific round.
 	 */
 	public void interpret(Round roundSequence){
-		
+		// do stuff to change score based on this round's new information
 	}
 	
-	/**
-	 * This method will accept two integers that will be compared to determine if and who won the game.
-	 * @param weScore The score of the 'we' team
-	 * @param theyScore The score of the 'they team'
-	 */
-	public void gameWinner(int weScore, int they){
-		
-	}
+
 }
