56f3654265f5f4dc2e9244b4e41664e3f738b484
kylekary <kylekary@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-08 17:48:57 +0000

    Added methods

--- src/euchre/player/GameManager.java
+++ src/euchre/player/GameManager.java
@@ -2,11 +2,38 @@ package euchre.player;
 
 public class GameManager {
 	
+	private Player player1, player2, player3, player4;
+	private Player dealer = player1;
+	
+	private Deck deck;
+	
 	
 	public static void main(String[] args) {
 		
+		
+	}
+	
+	public void deal(){
+		deck.shuffle();
+		
+		
+		nextDealer(dealer);
 	}
 
+	private void nextDealer(Player p) {
+		if(p==player1){
+			dealer = player2;
+		}
+		else if(p==player2){
+			dealer = player3;
+		}
+		else if(p==player3){
+			dealer = player4;
+		}
+		else{
+			dealer = player1;
+		}
+	}
 	
 	
 }
