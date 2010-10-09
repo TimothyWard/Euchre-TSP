7c301a12b0b08d0e844169c96261204ce1ae8ebc
matt.helgen <matt.helgen@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-09 19:52:46 +0000

    Added comments. Server will listen for connections, and the client can connect and send a message. The client needs the server's hostname hardcoded in ClientNetworkManager in order to function.

--- trunk/src/euchre/network/TestClass.java
+++ trunk/src/euchre/network/TestClass.java
@@ -4,32 +4,39 @@ package euchre.network;
 public class TestClass {
 
 	/**
+	 * Used to initiate / test the network and client connections
+	 * 
 	 * @param args
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		
-		
 
-		
-		
-		
-		
-		//ServerNetworkManager server = new ServerNetworkManager();
-		//server.start();
-		
-		ClientNetworkManager client = new ClientNetworkManager();
-		client.start();
-		
+
+		boolean isClient = true;
+
+
+		if(isClient){
+			//create the client thread and start it
+			ClientNetworkManager client = new ClientNetworkManager();
+			client.start();
+		}
+		else{
+			//create the server thread and start it
+			ServerNetworkManager server = new ServerNetworkManager();
+			server.start();
+		}
+
+
+
 		/*
-		
+
 		for(int i = 0; i<1000; i++)
 		{
 			System.out.println("Main outputting");
 			Thread.sleep(500);
 		}
-		*/
-		
+		 */
+
 	}
 
 }
