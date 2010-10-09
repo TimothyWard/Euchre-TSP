8c8212c0ddf091862a28702919f9df6bd9877697
unwrittenrainbow <unwrittenrainbow@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-09 19:42:37 +0000

    added classes to new game package

---
+++ trunk/src/euchre/game/GameLogic.java
@@ -0,0 +1,20 @@
+package euchre.game;
+/**
+ * 
+ * @author Timothy Ward
+ * 
+ * This class is solely responsible for game operations that pertain to the logic of the game.
+ *
+ */
+public class GameLogic{
+
+	/**
+	 * This method accepts a round object and interprets it relative to the game.
+	 * @param roundSequence The round object containing all information for a specific round.
+	 */
+	public void interpret(Round roundSequence){
+		// do stuff to change score based on this round's new information
+	}
+	
+
+}
