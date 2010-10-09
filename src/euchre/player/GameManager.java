b54438bee24e18ebd749f9ccf36e3d80b37f80eb
kylekary <kylekary@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-09 00:32:51 +0000

    Method updates

--- src/euchre/player/GameManager.java
+++ src/euchre/player/GameManager.java
@@ -1,39 +1,61 @@
 package euchre.player;
 
 public class GameManager {
-	
+
 	private Player player1, player2, player3, player4;
 	private Player dealer = player1;
-	
+
 	private Deck deck;
-	
-	
+
+
 	public static void main(String[] args) {
-		
-		
-	}
-	
-	public void deal(){
-		deck.shuffle();
-		
-		
-		nextDealer(dealer);
+
+
 	}
 
-	private void nextDealer(Player p) {
+	public void deal(){
+		
+		deck.shuffle();									//Shuffle the deck of cards
+		int draw=2;										//The number of cards to deal a player
+		Player curPlayer = dealer;
+
+		for(int i=0;i<8;i++){							//Deals to each of the players twice
+			curPlayer=nextPlayer(curPlayer);
+			for(int x=0;x<draw;x++){					//Deals the appropriate number of cards to each player
+				curPlayer.drawCard(deck.drawCard());	
+			}
+			
+			if(draw==2){								//If the previous player was dealt 2 cards,
+				draw=3;									//deal the next player 3 cards, and vice versa
+			}
+			else{
+				draw=2;
+			}
+
+		}
+
+		dealer=nextPlayer(dealer);
+	}
+
+	/**
+	 * Finds the next player after a given player
+	 * @param p Given player
+	 * @return The player after the given player
+	 */
+	private Player nextPlayer(Player p) {
 		if(p==player1){
-			dealer = player2;
+			return player2;
 		}
 		else if(p==player2){
-			dealer = player3;
+			return player3;
 		}
 		else if(p==player3){
-			dealer = player4;
+			return player4;
 		}
 		else{
-			dealer = player1;
+			return player1;
 		}
 	}
-	
-	
+
+
 }
