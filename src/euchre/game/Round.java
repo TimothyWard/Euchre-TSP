d6c105e73da270078428bc69e6b5686725de21d1
unwrittenrainbow <unwrittenrainbow@1c23b01f-6d0d-b85a-8384-b684a3189a81>
2010-10-09 19:42:37 +0000

    added classes to new game package

---
+++ src/euchre/game/Round.java
@@ -0,0 +1,19 @@
+package euchre.game;
+import euchre.player.*;
+/**
+ * 
+ * @author Timothy Ward
+ * 
+ * This class holds all information pertaining to a single round. The only class in communication with this one is the
+ * Game class. The game class writes information to this class as it becomes available from network. This object is then
+ * passed to GameLogic, which interprets and passes back pertinent information to Game.
+ *
+ */
+public class Round{
+	protected char trumpSuit = ' ';
+	protected Player playerWhoOrdered;
+	protected char teamWhoOrdered = ' ';
+	protected char suitLed = ' ';
+	protected boolean alone = false;
+	protected Card[] cardsPlayed = new Card[20];
+}
